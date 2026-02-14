4bfb107067b9ea59aff74ecad8a98a4840636b0c
NJI HALLE CHO-NKWENTI <njihalle10@gmail.com>
2026-02-14 07:52:43 +0100

    Add files via upload

---
+++ frontend/navigation/AuthNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/Auth/LoginScreen';
+import RegisterScreen from '../screens/Auth/RegisterScreen';
+
+const AuthStack = createStackNavigator();
+
+const AuthNavigator = () => {
+  return (
+    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+      <AuthStack.Screen name="Login" component={LoginScreen} />
+      <AuthStack.Screen name="Register" component={RegisterScreen} />
+    </AuthStack.Navigator>
+  );
+};
+
+export default AuthNavigator;
